7bd9b225c95a256858287dd8848d7e85adb49bce
Tianyu Liu <liu.tianyu93@hotmail.com>
2022-09-05 22:19:46 +0200

    Air sensor is in

--- src/devices/airsensor.cpp
+++ src/devices/airsensor.cpp
@@ -5,22 +5,47 @@
 #include "../communication/mqtt.h"
 
 constexpr uint16_t MSG_SIZE = 2048;
+const String statusTopic = "studiotj/greenhouse/air_data/status";
+const String availabilityTemplate = "{{ value_json.availability }}";
+
+const struct {
+    const String uniqueId = "airsensor_temperature_" + WiFi.macAddress();
+    const String name = "greenhouse_temperature";
+    const String discoveryTopic = "homeassistant/sensor/greenhouse/temperature/config";
+    const String baseTopic = "studiotj/greenhouse/air_data";
+    const String deviceClass = "temperature";
+    const String stateClass = "measurement";
+    const String unitOfMeasurement = "°C";
+    const String valueTemplate = "{{ value_json.temperature }}";
+
+} TemperatureSensorInfo;
+
+const struct {
+    const String uniqueId = "airsensor_humidity_" + WiFi.macAddress();
+    const String name = "greenhouse_humidity";
+    const String discoveryTopic = "homeassistant/sensor/greenhouse/humidity/config";
+    const String deviceClass = "humidity";
+    const String stateClass = "measurement";
+    const String unitOfMeasurement = "%";
+    const String valueTemplate = "{{ value_json.humidity }}";
+
+} HumiditySensorInfo;
 
 HDC1080 AirSensor::temperatureSensor = HDC1080();
 
-void AirSensor::publishConfig() {
+void AirSensor::publishTemperatureSensorConfig() {
     StaticJsonDocument<MSG_SIZE> configInfo;
     constructDeviceInfo(&configInfo);
     configInfo["unique_id"] = TemperatureSensorInfo.uniqueId;
     configInfo["name"] = TemperatureSensorInfo.name;
-    configInfo["json_attributes_topic"] = TemperatureSensorInfo.statusTopic;
+    configInfo["json_attributes_topic"] = statusTopic;
 
-    configInfo["availability_topic"] = TemperatureSensorInfo.statusTopic;
-    configInfo["availability_template"] = TemperatureSensorInfo.availabilityTemplate;
+    configInfo["availability_topic"] = statusTopic;
+    configInfo["availability_template"] = availabilityTemplate;
 
     configInfo["device_class"] = TemperatureSensorInfo.deviceClass;
     configInfo["state_class"] = TemperatureSensorInfo.stateClass;
-    configInfo["state_topic"] = TemperatureSensorInfo.statusTopic;
+    configInfo["state_topic"] = statusTopic;
     configInfo["unit_of_measurement"] = TemperatureSensorInfo.unitOfMeasurement;
     configInfo["value_template"] = TemperatureSensorInfo.valueTemplate;
 
@@ -28,16 +53,46 @@ void AirSensor::publishConfig() {
     serializeJson(configInfo, outJson);
 
     MQTT::client.publish(TemperatureSensorInfo.discoveryTopic.c_str(), outJson.c_str(), true);
+}
+
+void AirSensor::publishHumiditySensorConfig() {
+    StaticJsonDocument<MSG_SIZE> configInfo;
+    constructDeviceInfo(&configInfo);
+    configInfo["unique_id"] = HumiditySensorInfo.uniqueId;
+    configInfo["name"] = HumiditySensorInfo.name;
+    configInfo["json_attributes_topic"] = statusTopic;
+
+    configInfo["availability_topic"] = statusTopic;
+    configInfo["availability_template"] = availabilityTemplate;
+
+    configInfo["device_class"] = HumiditySensorInfo.deviceClass;
+    configInfo["state_class"] = HumiditySensorInfo.stateClass;
+    configInfo["state_topic"] = statusTopic;
+    configInfo["unit_of_measurement"] = HumiditySensorInfo.unitOfMeasurement;
+    configInfo["value_template"] = HumiditySensorInfo.valueTemplate;
+
+    String outJson;
+    serializeJson(configInfo, outJson);
+
+    MQTT::client.publish(HumiditySensorInfo.discoveryTopic.c_str(), outJson.c_str(), true);
+}
+
+void AirSensor::publishConfig() {
+    publishTemperatureSensorConfig();
+    publishHumiditySensorConfig();
 
     readAndPublish();
 }
 
 void AirSensor::readAndPublish() {
     StaticJsonDocument<512> stateInfo;
-    int temperature = (int)(temperatureSensor.measureTemperature() * 100);
-    stateInfo["temperature"] = (double)temperature / 100;
+    AirData airData = temperatureSensor.measureTempAndHum();
+    int temperatureInt = (int)(airData.temperature * 100);
+    int humidityInt = (int)(airData.humidity * 100);
+    stateInfo["temperature"] = (double)temperatureInt / 100;
+    stateInfo["humidity"] = humidityInt;
     stateInfo["availability"] = temperatureSensor.deviceIsAvailable()? Availability.available : Availability.notAvailable;
     String outJson;
     serializeJson(stateInfo, outJson);
-    MQTT::client.publish(TemperatureSensorInfo.statusTopic.c_str(), outJson.c_str(), true);
+    MQTT::client.publish(statusTopic.c_str(), outJson.c_str(), true);
 }
--- src/devices/airsensor.h
+++ src/devices/airsensor.h
@@ -4,24 +4,13 @@
 
 #include "hdc1080.h"
 
-const struct {
-    const String uniqueId = "airsensor_temperature_" + WiFi.macAddress();
-    const String name = "greenhouse_temperature";
-    const String discoveryTopic = "homeassistant/sensor/greenhouse/temperature/config";
-    const String baseTopic = "studiotj/greenhouse/temperature";
-    const String statusTopic = "studiotj/greenhouse/temperature/status";
-    const String availabilityTemplate = "{{ value_json.availability }}";
-    const String deviceClass = "temperature";
-    const String stateClass = "measurement";
-    const String unitOfMeasurement = "°C";
-    const String valueTemplate = "{{ value_json.temperature }}";
-
-} TemperatureSensorInfo;
-
 class AirSensor {
 public:
     static void publishConfig();
     static void readAndPublish();
 private:
     static HDC1080 temperatureSensor;
+
+    static void publishTemperatureSensorConfig();
+    static void publishHumiditySensorConfig();
 };
